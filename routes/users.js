507358ea618305d24b48faef9c0963feb4a646a8
Robert Aydinyan <robert.aydinyan2002@gmail.com>
2019-06-25 19:14:21 +0400

    create update-user

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const mongojs = require('mongojs');
 
 // Get all users
 router.get('/users/', (req, res) => {
@@ -10,6 +11,22 @@ router.get('/users/', (req, res) => {
             res.json(users);
         }
     });
+});
+
+router.get('/update-user/:id', (req, res) => {
+    req.db.users.update(
+        {id: mongojs.ObjectId(req.params["id"])},
+        {$set: req.body},
+        (err, res) => {
+            if (err) {
+                err.errorMessage = 'unable to update user';
+                res.status(500).send(err);
+            }
+            else {
+                res.successMessage = 'user updated';
+                res.json(res);
+            }
+        });
 });
 
 module.exports = router;
