b2246ca7c9adbcb1a16a26e874e4d3c7e57f65cb
lilysimon351 <shushan.simonyan.03@gmail.com>
2019-06-25 19:09:01 +0400

    Create delete user api

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const mongojs = require('mongojs');
 
 // Get all users
 router.get('/users/', (req, res) => {
@@ -8,6 +9,16 @@ router.get('/users/', (req, res) => {
             res.status(500).send(error);
         } else {
             res.json(users);
+        }
+    });
+});
+
+router.delete('/users/:id', (req, res) => {
+    req.db.users.remove( { _id: mongojs.ObjectId(req.params.id) }, (err, resp) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.json(resp);
         }
     });
 });
